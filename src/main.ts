4eb03c879396a46b0ebe613698dae88e47fb5731
邦邦邦邦 <15622356989@163.com>
2024-11-04 19:34:01 +0800

    deploy

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import { router } from "./router";
-import "./assets/fonts/HarmonyOS/harmonyos-fonts.scss";
-// import "./assets/fonts/HarmonyOS/harmonyos-fonts.less";
-
-createApp(App).use(router).mount("#app");
